27bc50eb66a16277c1b56563f64c4571653da5d9
vipransh <vipranshverma385@gmail.com>
2023-01-31 11:10:19 +0530

    added coupon route

---
+++ Routes/couponRoute.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import {isLoggedIn} from '../middlewares/auth.middleware.js'
+const couponRouter=express.Router();
+
+import {createCoupon, deactivateCoupon, deleteCoupon, getAllCoupon} from '../Controllers/coupons.controller.js'
+
+
+// create coupon route
+couponRouter.route("/createCoupon").post(isLoggedIn, createCoupon);
+
+// deactivate coupon route
+couponRouter.route("/deactivateCoupon/:couponId").put(isLoggedIn, deactivateCoupon);
+
+// delete coupon route
+couponRouter.route("/deleteCoupon/:couponId").delete(isLoggedIn, deleteCoupon);
+
+// get all coupon route
+couponRouter.route("/getAllCoupon").get(isLoggedIn, getAllCoupon);
+
+
+export default couponRouter;
+
--- Routes/indexRoute.js
+++ Routes/indexRoute.js
@@ -3,11 +3,13 @@ const mainRouter=express.Router();
 
 import userRouter from './userRoutes.js';
 import collectionRouter from './collectionRoute.js';
+import couponRouter from './couponRoute.js';
 
 
 
 mainRouter.use('/user', userRouter);
 mainRouter.use("/collection", collectionRouter);
+mainRouter.use("/coupon", couponRouter);
 
 
 export default mainRouter;
